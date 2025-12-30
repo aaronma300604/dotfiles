188b0ad41bab9d23d3352ddac4327dd13ebd06fc
Aarón Mayoral <aaronmayoralansias@gmail.com>
2025-12-30 19:49:54 +0100

    Added wallpapers and changed to VSCode instead of Code-OSS

--- i3/.config/i3/layouts/launch_ws3.sh
+++ i3/.config/i3/layouts/launch_ws3.sh
@@ -27,4 +27,4 @@ i3-msg "workspace $WS; append_layout $LAYOUT"
 
 kitty --class pkmn -e bash -c "pokemon-colorscripts -s -rn umbreon,porygon2,haxorus,aegislash --no-title; exec bash" &
 kitty --class fexpl -e bash -c "yazi; exec bash" &
-code-oss &
+code &
